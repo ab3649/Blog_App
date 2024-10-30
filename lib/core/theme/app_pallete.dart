c8249ac1b2f1266e288df7377e2c2f3a3b5468f5
Anshuman <ab3649@srmist.edu.in>
2024-10-30 16:15:17 +0530

    initial commit

---
+++ lib/core/theme/app_pallete.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppPallete {
+  static const Color backgroundColor = Color.fromRGBO(23, 26, 33, 1);
+  static const Color gradient1 = Color.fromRGBO(88, 100, 29, 1);
+  // static const Color gradient2 = Color.fromRGBO(108, 125, 71, 1);
+  static const Color gradient2 = Color.fromRGBO(49, 92, 43, 1);
+  static const Color gradient3 = Color.fromRGBO(212, 228, 188, 1);
+  static const Color borderColor = Color.fromRGBO(135, 151, 175, 1);
+  static const Color whiteColor = Colors.white;
+  static const Color greyColor = Colors.grey;
+  static const Color errorColor = Colors.redAccent;
+  static const Color transparentColor = Colors.transparent;
+}
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,19 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+        //if no color is passes as an argument then it uses this defautl color
+        borderSide: BorderSide(
+          color: color,
+          width: 1.5,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: AppPallete.backgroundColor,
+      inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.all(25),
+          enabledBorder: _border(),
+          focusedBorder: _border(AppPallete.gradient1)));
+}
